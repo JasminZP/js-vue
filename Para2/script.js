d478bac571d5b5e30e262629af1f6b8082ee1717
Zinchuk Pavlo <150339743+JasminZP@users.noreply.github.com>
2026-09-04 12:01:25 +0300

    Add files via upload

---
+++ Para2/script.js
@@ -0,0 +1,104 @@
+// > < >= <=
+//
+// let a,b ;
+// a = '1';
+// b = 1;
+// console.log(a >>> b);
+//
+
+// if (umova) {
+//     hrenb
+// }
+
+// let temp = Number(prompt("Enter temperatura vasha"));
+// let result;
+// if (temp < 0) {
+//     result = 'Holod pipec';
+// }
+// else if (temp > 0 && temp <= 25) {
+//     result = "normiks";
+// }
+// else if (temp > 25) {
+//     result = "Jara";
+// }
+// alert(result);
+
+// _________________________________________________
+
+// let p = Number(prompt("Chislo vvedit bud laska"));
+//
+// if (p % 2 == 0) {
+//     alert("Parne");
+// }
+// else if (p % 2 != 0) {
+//     alert("Ne parne");
+// }
+
+// ___________________________________________________
+
+// const login = "admin";
+// const password = "password";
+//
+// let user_login = prompt("Login");
+// let user_password = prompt("Password");
+//
+// if (user_login === login && user_password === password) {
+//     alert("Vvishov v systemu")
+// }
+// else if (user_login === login && user_password !== password) {
+//     alert("Nepravilni parol")
+// }
+// else {
+//     alert("Nepravilny login")
+// }
+
+// post 100
+// courier 200
+// pickup 0
+
+
+// let deliveryType = prompt('Skilki tobi rokiv?');
+// let cost = 0;
+// switch (deliveryType) {
+//     case 'post':
+//         cost = 100;
+//         break;
+//     case 'courier':
+//         cost = 200;
+//         break;
+//     case 'pickup':
+//         break;
+//     default:
+//         cost = "no pavlo";
+//         break;
+// }
+// alert(cost);
+
+// _____________________________________________________________
+// let p = Number(prompt("Enter a number"));
+// let t = '';
+// switch (p) {
+//     case 1:
+//         t = 'Ponedilok';
+//         break;
+//     case 2:
+//         t = 'Vivtorok';
+//         break;
+//     case 3:
+//         t = 'sereda';
+//         break;
+//     case 4:
+//         t = 'chetverg';
+//         break;
+//     case 5:
+//         t = 'patnica';
+//         break;
+//     case 6:
+//         t = 'subota';
+//         break;
+//     case 7:
+//         t = 'nedila';
+//         break;
+// }
+// alert(t)
+// ______________________________________________________________
