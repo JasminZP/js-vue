266a9d808c67b5c8c7bc2dcfdf453581e90e0c6c
Zinchuk Pavlo <150339743+JasminZP@users.noreply.github.com>
2026-09-09 11:09:23 +0300

    Add files via upload

---
+++ para3/script.js
@@ -0,0 +1,78 @@
+// let age = Number(prompt("Vash vik?"))
+// let reg = confirm("Zareesctrovani?")
+// if (age >= 18 && reg === true) {
+//     alert("Vhid pidtvergeno")
+// }
+// else {
+//     alert("Vhid zaboroneno")
+// }
+
+// let rol = prompt("What vasha posada?")
+// if (rol === "teacher" || rol === "admin") {
+//     alert("Vhid vikonano")
+// }
+// else {
+//     alert("Vhid zaboroneno")
+// }
+
+// let conf = confirm("Vi zareestrovani?")
+// let age;
+// if (conf === true) {
+//     age = Number(prompt("Skilki tobi rokiv?"))
+//     if (age >= 18) {
+//         alert("Vhid")
+//     }
+//     else {
+//         alert("Ne vhid, male")
+//     }
+// }
+// else {
+//     alert("Ne vhid, zareestruysa")
+// }
+
+// let age = Number(prompt("Vik"));
+// if (age <= 6 && age >= 0) {
+//     alert("Pups")
+// }
+// else if (age > 6 && age <= 13) {
+//     alert("Male")
+// }
+// else if (age > 13 && age < 18) {
+//     alert("Pidlitok")
+// }
+// else if (age >= 18) {
+//     alert("Dorosli")
+// } (po spadanuy bez i)
+
+// let age = Number(prompt("Vik"));
+//
+//
+// if (age > 16) {
+//     alert("Dostup e")
+// }
+// else if (age <= 16) {
+//     alert("Dozvil potribno");
+//     let dozvil = confirm("Dozvil")
+//     if (dozvil === true) {
+//         alert("Vhid")
+//     }
+//     else {
+//         alert("Nema")
+//     }
+// }
+
+// let nazva = prompt("Tovar");
+// let kil = Number(prompt("Kilkist tovaru"));
+// let suma = nazva * kil;
+// let zar = confirm("Vi zareestrovani?");
+// let promo = prompt("Vvedit promokod");
+// const promo2 = "salesale";
+// let vip = confirm("U vas e vip?");
+// if (suma >= 1000 && zar === true && (promo === "salesale" || vip === true)) {
+//     alert("Znijka e");
+// }
+// else {
+//     alert("Znijka nema");
+// }
+
+
